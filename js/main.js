9dea4a995e18c7aa78885515db1cb9f8d5dcdb0c
Trugamr <Trugamr@users.noreply.github.com>
2018-08-16 23:54:13 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+var overlay = document.getElementById('grad-overlay');
+var background = document.getElementById('container');
+var artwork = document.getElementById('artwork');
+var title = document.getElementById('title');
+var artist = document.getElementById('artist');
+var controls = document.getElementById('controls');
+var seekbar = document.getElementById('progress-bar');
+
+var lightVibrantColor = 'rgb(215, 43, 105)';
+var backgroundImageUrl ='../resources/images/backdrop_5.jpg';
+var artworkUrl = '../resources/images/art_5.jpg';
+var musicTitle = 'Cruel';
+var musicArtist = 'Foxes';
+
+function rgbToAlpha(rgbColor) {
+    var rgbaColor;
+    rgbaColor = rgbColor.replace(')', ', 0.8)');
+    rgbaColor = rgbaColor.replace('rgb', 'rgba');
+    return rgbaColor;
+}
+
+//changing gradient color
+overlay.style = "background: linear-gradient(rgba(0,0,0,0.5) , rgba(30,30,30,0.66)," + rgbToAlpha(lightVibrantColor) + ")";
+
+//changing background artist image
+background.style = 'background: url(\' ' + backgroundImageUrl + ' \'); ';
+
+//changing artwork
+artwork.src = artworkUrl;
+//adding displaced colored background to artowkr using box-shadow
+artwork.style = 'box-shadow: 7px 7px 0px 0px rgba(0, 0, 0, 0.16), 14px 14px 0px ' + lightVibrantColor + ';';
+
+//changing title and artist
+title.innerText = musicTitle;
+artist.innerText = musicArtist;
+
+//adding 3d effct/multiple shadows to music controls
+controls.style = 'filter: drop-shadow(1px 1px 0px ' + lightVibrantColor + ') drop-shadow(2px 2px 0px ' + lightVibrantColor + ');' //drop-shadow(3px 3px 0px ' + lightVibrantColor + ');';
+
+//changing seebar bg color
+seekbar.style.background = lightVibrantColor;
+
+//seeking to 
+function seek(e) {
+    this.seekbar.value = e.offsetX; 
+    console.log(e.offsetX);
+}
